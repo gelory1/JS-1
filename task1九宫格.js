3e41987178e2f7c6cd1016731ba61ed70b3139b9
gelory1 <917589697@qq.com>
2018-01-19 21:12:15 +0800

    JS-1

--- task1九宫格.js
+++ task1九宫格.js
@@ -4,7 +4,13 @@
 var s = document.getElementsByClassName("sss");
 
  function sta(){
-
+     var sss = document.getElementById("btn-start");
+     sss.onclick = function () {
+         window.clearTimeout(id2);
+         window.clearTimeout(id3);
+         ove();
+         sta();
+     }
      var arry = new Array();
 
 for(var i=0;i<10;i++){
@@ -19,24 +25,21 @@ var i2 = arry[t2];
  var t3 = Math.floor(Math.random()*7);
  var i3 = arry[t3];
  arry.splice(t3,1);
-
-
-
     function bgc(){
         var r=Math.floor(Math.random()*256);
         var g=Math.floor(Math.random()*256);
         var b=Math.floor(Math.random()*256);
         return "rgb("+r+','+g+','+b+")";
     }
-
     s[i1].style.backgroundColor = bgc();
     s[i2].style.backgroundColor = bgc();
     s[i3].style.backgroundColor = bgc();
-    window.setTimeout("ove()",999);
-    var id2 = window.setTimeout("sta()",1000);
     var ss = document.getElementById("btn-over");
+    var id3 = window.setTimeout("ove()",999);
+    var id2 = window.setTimeout("sta()",1000);
     ss.onclick = function (){
     window.clearTimeout(id2);
+    window.clearTimeout(id3);
     ove();
 }
 }
